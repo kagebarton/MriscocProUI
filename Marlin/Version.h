ba830dabf0c46f01cfda88c65175ab98935f6360
classicrocker883 <classicrocker883@users.noreply.github.com>
2023-05-13 12:11:36 +0000

    [cron] Bump distribution date (2023-05-13)

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,9 +41,9 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2023-05-05"
+//#define STRING_DISTRIBUTION_DATE "2023-05-13"
 
-#define STRING_DISTRIBUTION_DATE __DATE__
+#define STRING_DISTRIBUTION_DATE "2023-05-13"
 #define STRING_DISTRIBUTION_TIME __TIME__
 
 /**
