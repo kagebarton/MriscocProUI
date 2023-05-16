53127855e608bc968e66eae4a445afd7888ca6f4
classicrocker883 <andrewleduc88@yahoo.com>
2023-05-16 17:54:49 -0400

    config multiple probing

--- configurations/Voxelab Aquila/BLT/Configuration.h
+++ configurations/Voxelab Aquila/BLT/Configuration.h
@@ -1622,7 +1622,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 1    // Use a value of (1) with ProUIex, otherwise (2)
+#define MULTIPLE_PROBING 2    // Use a value of (1) with ProUIex, otherwise (2)
 //#define EXTRA_PROBING    1
 
 /**
