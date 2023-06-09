ae505bb05f0ecc8479053e43950f51894fb8a83c
classicrocker883 <andrewleduc88@yahoo.com>
2023-06-09 00:36:42 -0400

    Configs

--- configurations/BTT-SKR_Mini_E3_V3/ConfigurationUBL.h
+++ configurations/BTT-SKR_Mini_E3_V3/ConfigurationUBL.h
@@ -1853,7 +1853,7 @@
  * RAMPS-based boards use SERVO3_PIN for the first runout sensor.
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  */
-#define FILAMENT_RUNOUT_SENSOR            // MRiscoC Enabled runout sensor support
+#define FILAMENT_RUNOUT_SENSOR            // MRiscoC Enabled runout sensor support (2528 bytes of flash)
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define FIL_RUNOUT_ENABLED_DEFAULT false// Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   1          // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
