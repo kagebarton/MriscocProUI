80694eb1e76a6255f6f238276a852a3e2566e17a
Andrew <18502096+classicrocker883@users.noreply.github.com>
2023-06-09 17:06:18 -0400

    Update ConfigurationPro.h
    no lockscreen

--- configurations/Voxelab Aquila/UBL/ConfigurationPro.h
+++ configurations/Voxelab Aquila/UBL/ConfigurationPro.h
@@ -3384,7 +3384,7 @@
   #define HAS_PLOT 1            // Graph Temp as grid plot - PID/MPC Tuning (1624 bytes of flash)
   #define HAS_ESDIAG 1          // View End-stop switch continuity (560 bytes of flash)
   #define HAS_CGCODE 1          // Extra Gcode options (3320 bytes of flash)
-  #define HAS_LOCKSCREEN 1      // Simple lockscreen as to not accidentally change something (568 bytes of flash)
+  //#define HAS_LOCKSCREEN 1      // Simple lockscreen as to not accidentally change something (568 bytes of flash)
   #define HAS_SD_EXTENDER 1     // Enable to support SD card extender cables (48 bytes of flash)
   #define USE_GRID_MESHVIEWER 1 // Enable two mesh graph types : one (1728 bytes of flash)
   #define HAS_CUSTOM_COLORS 1   // Able to change display colors (2040 bytes of flash)
