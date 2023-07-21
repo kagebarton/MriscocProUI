9a08249de456dca25271cab82412ba5bcd342ad6
classicrocker883 <andrewleduc88@yahoo.com>
2023-07-21 14:04:16 -0400

    Update configs - UBL ProUI

--- configurations/Voxelab Aquila/UBL/ConfigurationPro.h
+++ configurations/Voxelab Aquila/UBL/ConfigurationPro.h
@@ -3412,7 +3412,7 @@
   #define HAS_CUSTOM_COLORS 1   // Able to change display colors (2040 bytes of flash)
   #define ALT_COLOR_MENU 0      // Color palette options >> 0 = Voxelab Default | 1 = Alternate Aquila | 2 = Ender3V2 Default
   #if ENABLED(AUTO_BED_LEVELING_UBL)
-    #define ACTIVATE_MESH_ITEM  // Active Mesh Leveling menu option (152 bytes of flash)
+    //#define ACTIVATE_MESH_ITEM// Active Mesh Leveling menu option (152 bytes of flash)
   #endif
   #if ENABLED(FILAMENT_RUNOUT_SENSOR) // (2528 bytes of flash)
     #define RUNOUT_TUNE_ITEM    // Filament Runout option in Tune Menu 
