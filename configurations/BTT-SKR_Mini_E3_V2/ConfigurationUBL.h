e2f71d80135fb149baa2bd639d5e00b789172e29
classicrocker883 <andrewleduc88@yahoo.com>
2023-10-13 21:34:00 -0400

    Update compile-configs.yml, add V301 support.

--- configurations/BTT-SKR_Mini_E3_V2/ConfigurationUBL.h
+++ configurations/BTT-SKR_Mini_E3_V2/ConfigurationUBL.h
@@ -85,7 +85,7 @@
  *
  * :[-1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-#define SERIAL_PORT 2  // BTT SKR MINI E3 TFT
+#define SERIAL_PORT -1  // BTT SKR MINI E3 TFT >> 2
 #define NO_AUTO_ASSIGN_WARNING  // Disable serial warnings
 
 /**
@@ -107,7 +107,7 @@
  * Currently Ethernet (-2) is only supported on Teensy 4.1 boards.
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
-//#define SERIAL_PORT_2 -1
+//#define SERIAL_PORT_2 -1  // BTT SKR MINI E3 TFT
 //#define BAUDRATE_2 250000   // :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000] Enable to override BAUDRATE
 
 /**
