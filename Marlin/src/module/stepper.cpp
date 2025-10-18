00c13243016524aced5187366547a44eb801cc9c
kagebarton <126085658+kagebarton@users.noreply.github.com>
2025-10-17 20:14:31 -0400

    Revert stepper.cpp changes
    Revert to Nov 2024 version of the file because it ruins high baud rate host printing resend rates for some reason.

--- Marlin/src/module/stepper.cpp
+++ Marlin/src/module/stepper.cpp
@@ -1504,12 +1504,6 @@ void Stepper::apply_directions() {
  */
 
 HAL_STEP_TIMER_ISR() {
-  #ifndef __AVR__
-    // Disable interrupts, to avoid ISR preemption while we reprogram the period
-    // (AVR enters the ISR with global interrupts disabled, so no need to do it here)
-    hal.isr_off();
-  #endif
-
   HAL_timer_isr_prologue(MF_TIMER_STEP);
 
   Stepper::isr();
@@ -1526,6 +1520,12 @@ HAL_STEP_TIMER_ISR() {
 void Stepper::isr() {
 
   static hal_timer_t nextMainISR = 0;  // Interval until the next main Stepper Pulse phase (0 = Now)
+
+  #ifndef __AVR__
+    // Disable interrupts, to avoid ISR preemption while we reprogram the period
+    // (AVR enters the ISR with global interrupts disabled, so no need to do it here)
+    hal.isr_off();
+  #endif
 
   // Program timer compare for the maximum period, so it does NOT
   // flag an interrupt while this ISR is running - So changes from small
@@ -1548,6 +1548,8 @@ void Stepper::isr() {
   // We need this variable here to be able to use it in the following loop
   hal_timer_t min_ticks;
   do {
+    // Enable ISRs to reduce USART processing latency
+    hal.isr_on();
 
     hal_timer_t interval = 0;
 
@@ -1563,10 +1565,6 @@ void Stepper::isr() {
             ftMotion_nextAuxISR = (STEPPER_TIMER_RATE) / 400;
           }
         }
-
-        // Enable ISRs to reduce latency for higher priority ISRs, or all ISRs if no prioritization.
-        hal.isr_on();
-
         interval = _MIN(nextMainISR, ftMotion_nextAuxISR);
         nextMainISR -= interval;
         ftMotion_nextAuxISR -= interval;
@@ -1593,9 +1591,6 @@ void Stepper::isr() {
         const bool is_babystep = (nextBabystepISR == 0);  // 0 = Do Babystepping (XY)Z pulses
         if (is_babystep) nextBabystepISR = babystepping_isr();
       #endif
-
-      // Enable ISRs to reduce latency for higher priority ISRs, or all ISRs if no prioritization.
-      hal.isr_on();
 
       // ^== Time critical. NOTHING besides pulse generation should be above here!!!
 
